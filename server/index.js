e70b43a05ca5c157bc2f3e456cc7563ecb8df131
Shray0611 <164644420+Shray0611@users.noreply.github.com>
2026-06-25 23:40:25 +0530

    Fix Mongo URI config

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ process.on('unhandledRejection', (err) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI, {
     serverSelectionTimeoutMS: 5000,
 })
     .then(() => console.log('✅ MongoDB Connected'))
